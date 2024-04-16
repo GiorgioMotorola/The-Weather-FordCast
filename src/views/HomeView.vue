3fb6e171ededbff924fe5351c123f7fb33494659
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2024-04-16 13:07:18 -0400

    search box now returns results

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,13 @@
     <div class="pt-4 mb-8 relative">
       <input type="text" v-model="searchQuery" @input="getSearchResults" placeholder="Search for a city or state" class="py-2 px-1 w-full bg-transparent border-b 
       focus:border-weather-secondary focus:outline-none 
-      focus:shadow-[0px_1px_0_0_#004E71]">
+      focus:shadow-[0px_1px_0_0_#004E71]" />
+      <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="mapboxSearchResults">
+        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
     </div>
   </main>
 </template>
@@ -23,7 +29,6 @@ const getSearchResults = () => {
     if (searchQuery.value !== "") {
       const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${apiKey}&types=place`);
       mapboxSearchResults.value = result.data.features;
-      console.log(mapboxSearchResults.value);
       return;
     }
     mapboxSearchResults.value = null;
