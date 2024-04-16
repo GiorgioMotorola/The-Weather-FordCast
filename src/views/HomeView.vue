589faf9f85993044f1d03d4e713d8201fa6e4951
Mike Weatherford <97712526+GiorgioMotorola@users.noreply.github.com>
2024-04-16 17:16:14 -0400

    added route for city and state

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,8 @@
         <p v-if="searchError">Sorry, something went wrong. Please try again.</p>
         <p v-if="!searchError && mapboxSearchResults.length === 0">No results match your query. Please try again.</p>
         <template v-else>
-          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)">
             {{ searchResult.place_name }}
           </li>
         </template>
@@ -22,6 +23,23 @@
 import { ref } from "vue";
 import axios from "axios";
 import apiKey from "./apiKey.js";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const previewCity = (searchResult) => {
+  console.log(searchResult);
+  const [city, state] = searchResult.place_name.split(',');
+  router.push({
+    name: 'cityView',
+    params: { state: state.replaceAll(" ", ""), city: city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    }
+  })
+};
 
 const searchQuery = ref("");
 const queryTimeout = ref(null);
